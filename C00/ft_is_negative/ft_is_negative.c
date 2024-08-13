99e64520437caba7c92bdea5611a2819eed78e12
marcos <marcosalvarezcalabria@gmail.com>
2024-08-13 16:41:26 +0100

    add ft_is_negative

---
+++ C00/ft_is_negative/ft_is_negative.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   <filename>                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: <marcosal>                                 +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: <date> by <marcosal>                     #+#    #+#             */
+/*   Updated: <date> by <username>                    ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+void ft_putchar (char c)
+{
+	write(1, &c, 1);
+}
+void	ft_is_negative(int n)
+{
+	if(n< 0)
+	ft_putchar('N');
+	else
+	ft_putchar('P');	
+}
+int  main (void)
+{
+	int n;
+	n = -5;
+
+	ft_is_negative(n);
+
+}
